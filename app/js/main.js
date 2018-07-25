8770422c684a0a8bdf448495b0ff8d3b2ebdcf8a
Mandelbrot Studio <mandelbrot@iMac-de-Mandelbrot-2.local>
2018-07-24 23:24:36 -0500

    Hero section: testing topics shifts.

--- app/js/main.js
+++ app/js/main.js
@@ -6,46 +6,46 @@
 
 
 // Switching topics with typewriter effect functions
-	var TxtType = function(el, toRotate, period) {
-	    this.toRotate = toRotate;
-	    this.el = el;
-	    this.loopNum = 0;
-	    this.period = parseInt(period, 10) || 3000;
-	    this.txt = '';
-	    this.tick();
-	    this.isDeleting = false;
-	};
+	// var TxtType = function(el, toRotate, period) {
+	//     this.toRotate = toRotate;
+	//     this.el = el;
+	//     this.loopNum = 0;
+	//     this.period = parseInt(period, 10) || 3000;
+	//     this.txt = '';
+	//     this.tick();
+	//     this.isDeleting = false;
+	// };
 
-	TxtType.prototype.tick = function() {
-	    var i = this.loopNum % this.toRotate.length;
-	    var fullTxt = this.toRotate[i];
+	// TxtType.prototype.tick = function() {
+	//     var i = this.loopNum % this.toRotate.length;
+	//     var fullTxt = this.toRotate[i];
 
-	    if (this.isDeleting) {
-	    	this.txt = fullTxt.substring(0, this.txt.length - 1);
-	    } else {
-	    	this.txt = fullTxt.substring(0, this.txt.length + 1);
-	    }
+	//     if (this.isDeleting) {
+	//     	this.txt = fullTxt.substring(0, this.txt.length - 1);
+	//     } else {
+	//     	this.txt = fullTxt.substring(0, this.txt.length + 1);
+	//     }
 
-	    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+	//     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-	    var that = this;
-	    var delta = 150 - Math.random() * 100;
+	//     var that = this;
+	//     var delta = 150 - Math.random() * 100;
 
-	    if (this.isDeleting) { delta /= 2; }
+	//     if (this.isDeleting) { delta /= 2; }
 
-	    if (!this.isDeleting && this.txt === fullTxt) {
-	        delta = this.period;
-	        this.isDeleting = true;
-	    } else if (this.isDeleting && this.txt === '') {
-	        this.isDeleting = false;
-	        this.loopNum++;
-	        delta = 500;
-	    }
+	//     if (!this.isDeleting && this.txt === fullTxt) {
+	//         delta = this.period;
+	//         this.isDeleting = true;
+	//     } else if (this.isDeleting && this.txt === '') {
+	//         this.isDeleting = false;
+	//         this.loopNum++;
+	//         delta = 500;
+	//     }
 
-	    setTimeout(function() {
-	    	that.tick();
-	    }, delta);
-	};
+	//     setTimeout(function() {
+	//     	that.tick();
+	//     }, delta);
+	// };
 
 
 
