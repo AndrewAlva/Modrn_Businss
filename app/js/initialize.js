875072dfdc07eb338cbe8996a9606f3bd479992c
Mandelbrot Studio <mandelbrot@iMac-de-Mandelbrot-2.local>
2018-08-18 22:38:14 -0500

    Replacing img with SVG code. Mobile done, starting tablet

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   // console.log('Initialized app');
 
-  require('./humans.js');
+  // require('./humans.js');
   require('./main.js');
 });
